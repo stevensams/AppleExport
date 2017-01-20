22c6289ae779cf5c1d976bf183f0f3633c2859e7
Steven Sams <steven.sams@bbc.co.uk>
2017-01-20 10:26:38 +0000

    Cleaned up swift class files

--- AppleExport/Export.swift
+++ AppleExport/Export.swift
@@ -12,183 +12,49 @@ import MediaPlayer
 
 class Export: NSObject {
 
-    /*
-    func authorise()
-    {
-        SKCloudServiceController.requestAuthorization({
-            (status: SKCloudServiceAuthorizationStatus) in
-            switch(status)
-            {
-            case .authorized: print("Access granted.")
-            case .denied, .restricted: print("Access denied or restricted.")
-            case .notDetermined: print("Access cannot be determined.")
-            }
-        })
+    func getPlaylist() -> MPMediaItemCollection {
         
-        switch SKCloudServiceController.authorizationStatus()
-        {
-        case .authorized: print("Access granted.")
-        case .notDetermined: print("Not determined") //requestAuthorization()
-        case .denied, .restricted: print("Access denied or restricted.")
+        let localstore = UserDefaults.standard
+        let playlistId = localstore.string(forKey: "playlist")
+        var playlistreturn: MPMediaItemCollection?
+        
+        if let playlist = localstore.string(forKey: "playlist") {
+            let query = MPMediaQuery.playlists()
+            let items = query.collections
+            
+            for item in items! {
+                
+                let stringID: String = String(describing: item.persistentID)
+                let playlistid: String = String(describing: playlist)
+                
+                NSLog("Store: \(stringID) Library: \(playlistid)")
+                
+                if(stringID == playlist) {
+                    NSLog("Playlist found in settings: \(item.value(forProperty: MPMediaPlaylistPropertyName))")
+                    playlistreturn = item
+                } else {
+                    // NSLog("not equal - \(String(item.persistentID) == playlist))")
+                    //NSLog(item.description)
+                    //NSLog(String(item.persistentID))
+                    ///NSLog("local store: \(playlist)")
+                }
+            }
+        } else {
+            NSLog("no playlist in local storage")
         }
-        
-        let controller = SKCloudServiceController()
-        
-      /*  controller.requestCapabilities(completionHandler: {
-            (capabilities: SKCloudServiceCapability, error: NSError?) in
-            if capabilities.contains(SKCloudServiceCapability.musicCatalogPlayback)
-            {
-                print("The device allows playback of Apple Music catalog tracks.")
-            }
-        } as! (SKCloudServiceCapability, Error?) -> Void)
-        */
-        controller.requestCapabilities(completionHandler: {
-            (capabilities: SKCloudServiceCapability, error: NSError?) in
-            if capabilities.contains(SKCloudServiceCapability.addToCloudMusicLibrary)
-            {
-                print("The device allows tracks to be added to the user’s music library.")
-            } else {
-                print("capabilities \(capabilities)")
-            }
-        } as! (SKCloudServiceCapability, Error?) -> Void)
+        return playlistreturn!
     }
-    */
-    func authorise() {
-        
-       // var error = false
-        
-        SKCloudServiceController.requestAuthorization({
-            (status: SKCloudServiceAuthorizationStatus) in
-            switch status {
-            case .denied:
-                DispatchQueue.main.async {
-                    print("SKCloud Authorisation error")
-                    //error = true
-                }
-            case .restricted:
-                print("SKCloud Restricted \(status)")
-                // Access to iCloud restricted
-            default:
-                print("SKCloud Authorisation success")
-                break
-            }
-        })
-        
-        switch SKCloudServiceController.authorizationStatus()
-        {
-        case .authorized: checkCapabilities()
-        case .notDetermined: print("not determined")//checkCapabilities()
-        case .denied, .restricted: print("restricted")//checkCapabilities()
-        }
-        
-        /*
-        
-        let controller = SKCloudServiceController()
-        controller.requestCapabilities(completionHandler: {
-            (capabilities: SKCloudServiceCapability, error: NSError?) in
-            if capabilities.contains(SKCloudServiceCapability.addToCloudMusicLibrary)
-            {
-                print("The device allows addToCloudMusicLibrary.")
-            }
-        } as! (SKCloudServiceCapability, Error?) -> Void)
-        */
-        /*
-         func iCloudAuthStatus() {
-         switch SKCloudServiceController.authorizationStatus()
-         {
-         case .authorized: print("authorized")
-         case .notDetermined:
-         requestICloudAccess()
-         break
-         case .restricted: print("restricted")//checkCapabilities()
-         case .denied: print("restricted")//checkCapabilities()
-         }
-         }
-         
-         func requestICloudAccess() {
-         SKCloudServiceController.requestAuthorization({
-         (status: SKCloudServiceAuthorizationStatus) in
-         switch status {
-         case .denied:
-         print("SKCloud Authorisation error")
-         case .restricted:
-         print("SKCloud Restricted \(status)")
-         // Access to iCloud restricted
-         default:
-         print("SKCloud Authorisation success")
-         break
-         }
-         })
-         }
-         */
-        
-        MPMediaLibrary.requestAuthorization { (status) in
-            if status == .authorized {
-                print("MPMediaLibrary authorised")
-                self.runMediaLibraryQuery()
-            } else {
-                switch MPMediaLibrary.authorizationStatus() {
-                case .restricted:
-                    print("Media library access restricted by corporate or parental settings")
-                case .denied:
-                    print("Media library access denied by user")
-                default:
-                    print("Unknown error")
-                }
-            }
-        }
-        
-        
-       // let player = MPMusicPlayerController.systemMusicPlayer()
-       // player.setQueueWithStoreIDs(<#T##storeIDs: [String]##[String]#>)
-        
  
-       // return error
-    }
-    
-    func runMediaLibraryQuery() {
-        let query = MPMediaQuery.songs()
-        if let items = query.items, let item = items.first {
-            print("Title: \(item.title)")
-        }
-    }
-    
-    func checkCapabilities() {
+    func constructPlaylist() {
         
-        
-        SKCloudServiceController().requestCapabilities { capabilities, error in
-            if error != nil {
-              
-                    print("SKCloud Capabilities error")
-                    //error = true
-                
-            } else if capabilities.contains(SKCloudServiceCapability.addToCloudMusicLibrary) {
-                //https://developer.xamarin.com/api/field/StoreKit.SKCloudServiceCapability.AddToCloudMusicLibrary/
-                print("SKCloud Capabilities success: \(capabilities)")
-            } else {
-                // if 2 = register with apple music - restricted
-                //if 1 = sharing disabled - shazam
-                
-                print("SKCloud Capabilities cannot write tracks: \(capabilities)")
-            }
-        }
-        
-    }
-    
-    func addTrack(productID: String) {
-        
-        
-    }
-    
-    func getPlaylist(playlistID: String) {
-     //   MPMusicPlayerController.
-    }
-    
-    func createPlaylist() {
-        
+        let localstore = UserDefaults.standard
         let library = MPMediaLibrary.default()
         let metadata = MPMediaPlaylistCreationMetadata(name: "My BBC Music Playlist")
-        metadata.descriptionText = "Exported on DATE"
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "dd/MM/yyyy HH:MM"
+        let dateString = format.string(from: date)
+        metadata.descriptionText = "Exported on \(dateString)"
         metadata.authorDisplayName = "BBC Music"
         
         library.getPlaylist(with: UUID(), creationMetadata: metadata) { playlist, error in
@@ -196,7 +62,6 @@ class Export: NSObject {
                 DispatchQueue.main.async {
                     print("Error writing playlist")
                 }
-                return
             } else {
                 let delayTime = DispatchTime.now() + Double(Int64(5.0 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
                 /* We need to wait ~5 seconds for a new playlist to be ready for adding tracks
@@ -204,39 +69,29 @@ class Export: NSObject {
                  */
                 DispatchQueue.main.asyncAfter(deadline: delayTime) {
                     
-                    print("Playlist written \(playlist?.persistentID)")
-                    
-                    ///
-                    /*
-                     playlist?.addItem(withProductID: "712325420") { error in
-                        if error != nil {
-                            print("error writing track \(error)")
-                            return
-                        } else {
-                            print("Added track to playlist")
-                        }
-                     }
-                     */
-                    
-                    ///
-                    
-                    //playlist
-                    
+                    let stringID = playlist!.persistentID.description
+                    localstore.set(stringID, forKey: "playlist")
+                    localstore.synchronize()
+                    NSLog("\(stringID) written")
                 }
                 
             }
         }
     }
     
-    func addTracktoPlaylist() {
-        let player = MPMusicPlayerController.systemMusicPlayer()
-        player.setQueueWithStoreIDs(["401187150"])
-        player.play()
+    func addTrack(playlist: MPMediaPlaylist, track: String) -> Bool {
         
-       // let library = MPMediaLibrary.default()
-       // let metadata = MPMediaPlaylistCreationMetadata(name: "My BBC Music Playlist")
-        
-       // library.getPlaylist(with: <#T##UUID#>, creationMetadata: <#T##MPMediaPlaylistCreationMetadata?#>, completionHandler: <#T##(MPMediaPlaylist?, Error?) -> Void#>)
-       
+        var status = false
+        playlist.addItem(withProductID: track) { error in
+            if error != nil {
+                print("error writing track \(error)")
+                
+            } else {
+                print("Added track to playlist")
+                status = true
+            }
+        }
+        print("playlist writing done \(playlist.persistentID)")
+        return status
     }
 }
--- AppleExport/ViewController.swift
+++ AppleExport/ViewController.swift
@@ -12,10 +12,47 @@ import MediaPlayer
 
 class ViewController: UIViewController {
     
-    let iTunesTracks:[String] = ["695543530","324212499","585972803","1173028439","1050244131","264373418","159363080","269864994","300972325","388127880","257541624","419770556","725824503","398138385","659741383","401572692","59081939","429945616","1095449161","429945814","1173106804","1104984917","704273366","1171369368","1131561770","640369648","1160747530","714632733","388127922","1135984045","1109473878","388128572","580708180","7593728","1174605458","611387719","714861225","1175357008","928248075","1153701506","1137330136","1132115989","1051333363","14193723","1179447252","1168957626","1169385965","712325420","388128567","1174559876","1168039947","1156443908","14193705","1150317110","1170657803","1171368622","1169246072","1164046335","1155229943","1178024930","1123546970","1179022952","15466647","206627085","329519371","1133221371","14976844","724391063","1171239668","891452599","681348468","929802289","1152499331","867694916","963510673","296790657","1154504662","1079131941","1064886123","1165164775","29215363","1138293961","692299824","1160511318","1100287328","7668336","1151464059","941830995","1160511315","1151197755","1097758930","1152996330"]
     
+    @IBAction func createPlaylist() {
+        let export = Export()
+        let playlist = export.getPlaylist()
+        
+      /*  if(playlist == nil) {
+            // no playlist, create one?
+        } else {
+            //show playlist
+        }*/
+    }
+    
+    @IBAction func openPlaylist() {
+        //let iTunesPath = "https://itunes.apple.com/gb/playlist/my-bbc-music-playlist/idpl.f96eb1bdb34f4e779ee97f1a87b651c3"
+        let iTunesPath = "https://itunes.apple.com/gb/playlist/my-bbc-music-playlist/idpl.6939409701285735539"
+        let settingsUrl = NSURL(string: iTunesPath)
+        UIApplication.shared.open(settingsUrl as! URL)
+    }
+    
+    @IBAction func addTrack() {
+        let export = Export()
+        let playlist = export.getPlaylist()
+        
+        let utils = Utils()
+        let track = utils.getRandomTrack()
+        let status = export.addTrack(playlist: playlist as! MPMediaPlaylist, track: track)
+        
+        if(status == true) {
+            NSLog("OK, added track \(track)")
+        } else {
+            NSLog("Not ok, no track added \(track)")
+        }
+    }
+    
+    /** Authentication **/
     
     @IBAction func authenticate() {
+        self.processAuthorisation()
+    }
+    
+    func processAuthorisation() {
         if(self.isDeviceCapable()) {
             if(self.requestICloudAccess()) {
                 if(self.isUKStorefront()) {
@@ -28,80 +65,10 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func createPlaylist() {
-        self.constructPlaylist()
-    }
-    
-    @IBAction func addTrack() {
-        
-        /*
-        for trackId in self.iTunesTracks {
-            playlist?.addItem(withProductID: trackId) { error in
-                if error != nil {
-                    print("error writing track \(error)")
-                    return
-                } else {
-                    NSLOG("Added track to playlist")
-                }
-            }
-        }
-        NSLOG("playlist writing done \(playlist?.persistentID)")
-        
-        self.exportSuccess(count: self.iTunesTracks.count)
-        */
-    }
-    
-    @IBAction func openPlaylist() {
-
-        //let iTunesPath = "https://itunes.apple.com/gb/playlist/my-bbc-music-playlist/idpl.f96eb1bdb34f4e779ee97f1a87b651c3"
-       // let iTunesPath = "https://itunes.apple.com/gb/playlist/my-bbc-music-playlist/idpl.6939409701285735539"
-       // let settingsUrl = NSURL(string: iTunesPath)
-       // UIApplication.shared.open(settingsUrl as! URL)
-    }
-    
-    func isDeviceCapable() -> Bool {
-        
-        var capable = false
-        SKCloudServiceController().requestCapabilities { capabilities, error in
-            if error != nil {
-                NSLog("SKCloudServiceController Capabilities error")
-                
-            } else if capabilities.contains(SKCloudServiceCapability.addToCloudMusicLibrary) {
-                //https://developer.xamarin.com/api/field/StoreKit.SKCloudServiceCapability.AddToCloudMusicLibrary/
-                NSLog("SKCloud Capabilities success: User 'is capable' of writing to their Apple Music Library")
-                capable = true
-            } /*else if (capabilities & SKCloudServiceCapabilityMusicCatalogPlayback) {
-                // http://stackoverflow.com/questions/37613889/apple-music-detect-is-member
-                // https://developer.xamarin.com/api/type/StoreKit.SKCloudServiceAuthorizationStatus/
-                // concerned with playback
-            } */ else if capabilities.rawValue == 2 {
-                //if 2 = register with apple music - restricted
-                //if 1 = sharing disabled - shazam
-                self.promptAppleMusicSubscriptionNeeded()
-            } else if capabilities.rawValue == 1 {
-                self.promptICloudAccessRequired()
-            } else {
-                NSLog("SKCloud Capabilities cannot write tracks: \(capabilities)")
-            }
-        }
-        return capable
-    }
-    
-    func clearPrefs() {
-     
-        let localstore = UserDefaults.standard
-        localstore.removeObject(forKey: "playlist")
-        localstore.synchronize()
-        NSLog("playlist removed")
-    }
-    
     func requestICloudAccess() -> Bool {
-        
         var capable = false
-        
         SKCloudServiceController.requestAuthorization({
             (status: SKCloudServiceAuthorizationStatus) in
-            
             switch status {
             case .authorized:
                 print("SKCloud Authorisation authorized")
@@ -114,7 +81,6 @@ class ViewController: UIViewController {
                 print("SKCloud Restricted \(status)")
             case .notDetermined:
                 print("SKCloud Restricted \(status)")
-                //break
             }
         })
         return capable
@@ -148,71 +114,35 @@ class ViewController: UIViewController {
         return capable
     }
     
-    func getPlaylist() -> MPMediaItemCollection {
+    
+    func isDeviceCapable() -> Bool {
         
-        let localstore = UserDefaults.standard
-        let playlistId = localstore.string(forKey: "playlist")
-        var playlist: MPMediaItemCollection?
-        
-        if (playlistId != nil) {
-        
-            let predicate = MPMediaPropertyPredicate(value: playlistId, forProperty: MPMediaItemPropertyPersistentID)
-            let query = MPMediaQuery()
-            query.addFilterPredicate(predicate)
-        
-            if let items = query.collections, let item = items.first {
+        var capable = false
+        SKCloudServiceController().requestCapabilities { capabilities, error in
+            if error != nil {
+                NSLog("SKCloudServiceController Capabilities error")
                 
-                playlist = items
-                NSLog("Playlist IDs: \(playlistId) and \(item.persistentID)")
-                NSLog("Playlist found in settings: \(item.value(forProperty: MPMediaPlaylistPropertyName))")
-                NSLog("rep value: \(playlist.items)")
-                NSLog("rep value: \(playlist.representativeItem)")
-                returnPlaylist = playlist.representativeItem!
+            } else if capabilities.contains(SKCloudServiceCapability.addToCloudMusicLibrary) {
+                //https://developer.xamarin.com/api/field/StoreKit.SKCloudServiceCapability.AddToCloudMusicLibrary/
+                NSLog("SKCloud Capabilities success: User 'is capable' of writing to their Apple Music Library")
+                capable = true
+            } /*else if (capabilities & SKCloudServiceCapabilityMusicCatalogPlayback) {
+                 // http://stackoverflow.com/questions/37613889/apple-music-detect-is-member
+                 // https://developer.xamarin.com/api/type/StoreKit.SKCloudServiceAuthorizationStatus/
+             } */ else if capabilities.rawValue == 2 {
                 
+                //if 2 = register with apple music - restricted
+                //if 1 = sharing disabled - shazam
+                self.promptAppleMusicSubscriptionNeeded()
+            } else if capabilities.rawValue == 1 {
+                self.promptICloudAccessRequired()
             } else {
-                NSLog("no playlist found through predicate")
+                NSLog("SKCloud Capabilities cannot write tracks: \(capabilities)")
             }
-        } else {
-            NSLog("no playlist in local storage")
-            self.constructPlaylist()
-            return self.getPlaylist()
         }
-        return playlist!
+        return capable
     }
     
-    func constructPlaylist() {
-        
-        let localstore = UserDefaults.standard
-        let library = MPMediaLibrary.default()
-        let metadata = MPMediaPlaylistCreationMetadata(name: "My BBC Music Playlist")
-        let date = Date()
-        let format = DateFormatter()
-        format.dateFormat = "dd/MM/yyyy HH:MM"
-        let dateString = format.string(from: date)
-        metadata.descriptionText = "Exported on \(dateString)"
-        metadata.authorDisplayName = "BBC Music"
-        
-        library.getPlaylist(with: UUID(), creationMetadata: metadata) { playlist, error in
-            if error != nil {
-                DispatchQueue.main.async {
-                    print("Error writing playlist")
-                }
-            } else {
-                let delayTime = DispatchTime.now() + Double(Int64(5.0 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-                /* We need to wait ~5 seconds for a new playlist to be ready for adding tracks
-                 https://forums.developer.apple.com/message/140472#140472
-                 */
-                DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                    
-                    let stringID = playlist!.persistentID.description
-                    localstore.set(stringID, forKey: "playlist")
-                    localstore.synchronize()
-                    NSLog("\(stringID) written")
-                }
-                
-            }
-        }
-    }
     
     /* user prompts */
     
@@ -220,6 +150,7 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "Export Complete", message: "\(count) tracks added to your Apple Music Library", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         present(alertController, animated: true, completion: nil)
+        
     }
     
     func promptAuthSuccess() {
@@ -263,3 +194,4 @@ class ViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
 }
+
